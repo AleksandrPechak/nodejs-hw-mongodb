4d5644ddd15486949a9d39e89929bb6f791f8fc4
Aleksandr Pechak <alpechak@gmail.com>
2024-06-15 18:38:02 +0300

    Update

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -35,10 +35,4 @@ export const deleteContact = async (contactId) => {
   const result = ContactsCollection.findByIdAndDelete(contactId);
   return result;
 };
-
-// export const getAllContacts = () => ContactsCollection.find();
-// export const getContactById = (contactId) => ContactsCollection.findById(contactId);
-// export const createContact = (contactData) => ContactsCollection.create(contactData);
-// export const patchContact = (contactId, contactData) => ContactsCollection.findByIdAndDelate(contactId, contactData);
-// export const deleteContact = (contactId) => ContactsCollection.findByIdAndDelate(contactId);
  
