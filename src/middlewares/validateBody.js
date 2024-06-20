b46e227735df23657fdd5158e05f67bdd9315668
Aleksandr Pechak <alpechak@gmail.com>
2024-06-20 10:57:08 +0300

    update

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,11 +1,11 @@
-import createHttpError from "http-errors";
+import createHttpError from 'http-errors';
 
-export const validateBody =(schema) => async (req, res, next) => {
-        try {
-                await schema.validateAsinc(req.body, { abortEarly: false});
-                next();
-        } catch (err) {
-                const error = createHttpError(400, 'Bad request', {error: err.details });
-                next(error);
-        }
+export const validateBody = (schema) => async (req, res, next) => {
+  try {
+    await schema.validateAsinc(req.body, { abortEarly: false });
+    next();
+  } catch (err) {
+    const error = createHttpError(400, 'Bad request', { errors: err.details });
+    next(error);
+  }
 };
